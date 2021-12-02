cefe9b15488b5f59f86d94c472fd5a84717d35e3
MoDoStRoY <modostroy@gmail.com>
2021-12-02 20:05:24 +0500

    v.0

---
+++ ClientApp/Pages/CreateDocuments/CreateDocumentsPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="leftBlock">
+      <b-switch type="is-info">Заявление-декларация</b-switch>
+      <b-switch type="is-info">Акт измерения</b-switch>
+    </div>
+
+    <div class="centerBlock">
+      
+    </div>
+
+    <div class="rightBlock">
+
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {Component} from "vue-property-decorator";
+
+@Component
+export default class CreateDocumentsPage extends Vue
+{
+  name: string = "CreateDocumentsPage";
+  
+  
+}
+</script>
+
+<style>
+
+.leftBlock, .rightBlock
+{
+  width: 20%;
+  height: 100%;
+  float: left;
+}
+
+.centerBlock
+{
+  width: 60%;
+  height: 100%;
+  float: left;
+  margin: 0;
+}
+
+</style>
